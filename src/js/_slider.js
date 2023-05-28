ea91fda8b4ad35b70ec30edc7127db34fffd7469
sassy <luxeadd@gmail.com>
2023-05-28 22:38:40 +0900

    トップページレイアウト

--- src/js/_slider.js
+++ src/js/_slider.js
@@ -12,4 +12,23 @@ export function slider() {
       pauseOnHover: false, //スライダーにマウスホバーした時にスライドを停止させる
     });
   });
+
+  $(".js-campaignSlider").slick({
+    autoplay: true, //自動再生（初期値：false）
+    autoplaySpeed: 2000, //自動スライド間隔
+    speed: 1000, //スライドやフェードの速度（初期値：300）単位はミリ秒
+    variableWidth: true, //コンテンツの幅の維持して画面幅が広がれば要素数を増やす（CSSでコンテンツにwidthの設定が必要）
+    pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, //スライダーにマウスホバーした時にスライドを停止させるか
+    prevArrow: '<div class="c-slide-arrow c-prev-arrow"></div>',
+    nextArrow: '<div class="c-slide-arrow c-next-arrow"></div>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false, //左右の矢印表示（初期値：true）
+        },
+      },
+    ],
+  });
 }
